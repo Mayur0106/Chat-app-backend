8f8bcf9fe1dbfea6c221d93e35aaebbb108ac7a6
Mayur <mayurshinde0106@gmail.com>
2023-03-27 18:42:53 +0530

    One more thing

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: ["http://localhost:3000","https://chat-app.onrender.com"],
+    origin: ["http://localhost:3000","https://chat-app-dnur.onrender.com"],
     methods: ['GET', 'POST']
   }
 })
