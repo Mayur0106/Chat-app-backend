2265bdca90ce6b3590a0bd28f02b9f131f13eda5
Mayur Shinde <97580421+Mayur0106@users.noreply.github.com>
2023-04-12 17:25:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ function sortRoomMessagesByDate(messages){
     let date1 = a._id.split('/');
     let date2 = b._id.split('/');
 
-    date1 = date1[2] + date1[0] + date1[1]
-    date2 =  date2[2] + date2[0] + date2[1];
+    date1 = date1[2] + date1[1] + date1[0]
+    date2 =  date2[2] + date2[1] + date2[0];
 
     return date1 < date2 ? -1 : 1
   })
