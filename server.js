39582f864a25a4cd09e6ebd8567731702c04ad10
Mayur Shinde <97580421+Mayur0106@users.noreply.github.com>
2024-05-01 23:38:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = process.env.PORT || 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: ["http://localhost:3000","https://chat-app-dnur.onrender.com"],
+    origin: ["http://localhost:3000",`${process.env.CHAT_APP_CLIENT_URL}`],
     methods: ['GET', 'POST']
   }
 })
