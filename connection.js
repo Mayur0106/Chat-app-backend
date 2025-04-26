234737f9f99307431e88506767e65e30bdd766cb
Mayur Shinde <97580421+Mayur0106@users.noreply.github.com>
2025-04-26 11:36:25 +0530

    update connection stirng

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.8fhdaec.mongodb.net/chatAppMern?retryWrites=true&w=majority&appName=Cluster0`, ()=>{
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.jasausc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, ()=>{
     console.log('connected to mongodb')
 })
